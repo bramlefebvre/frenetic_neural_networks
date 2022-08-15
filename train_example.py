9baa2e8f8547c65121850ab865b4838f0c914201
Bram Lefebvre <bramlefebvre@gmail.com>
2022-08-15 22:15:29 +0200

    demos

--- train_example.py
+++ train_example.py
@@ -8,12 +8,12 @@ from step_2.training import train_starting_with_random_vertex_n_times
 
 def train_example():
     patterns = (frozenset({0}),)
-    tournament_and_patterns = generate_single_tournament_and_patterns(100, patterns)
-    exuberant_system = find_exuberant_system(tournament_and_patterns, True).exuberant_system
+    tournament_and_patterns = generate_single_tournament_and_patterns(50, patterns)
+    exuberant_system = find_exuberant_system(tournament_and_patterns, False).exuberant_system
     print('exuberant system found')
-    initial_dynamics = initialize_dynamics(exuberant_system, 7, 10, 1)
-    training_result = train_starting_with_random_vertex_n_times(initial_dynamics, LearningAlgorithm.LOOK_FORWARD_AND_ONLY_ONCE_PER_ARC, 0.5, 0.2, 300)
-    print(calculate_path(training_result.dynamics.rate_matrix, 1, 1).path)
+    initial_dynamics = initialize_dynamics(exuberant_system, 5, 1, 1)
+    training_result = train_starting_with_random_vertex_n_times(initial_dynamics, LearningAlgorithm.LOOK_FORWARD_AND_ONLY_ONCE_PER_ARC, 0.5, 0.2, 200)
+    # print(calculate_path(training_result.dynamics.rate_matrix, 1, 1).path)
     performance = calculate_performance(training_result.dynamics, 0.2, 100)
     print(performance)
 
