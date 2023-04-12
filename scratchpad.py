accc110f8e8f1911b58487b051bcf490b74eda4c
Bram Lefebvre <bramlefebvre@gmail.com>
2023-04-12 14:35:14 +0200

    vscode settings

--- scratchpad.py
+++ scratchpad.py
@@ -1,14 +1,3 @@
 
-from dataclasses import dataclass
-import numpy
-
-from daos.graphs_and_patterns_dao import generate_single_graph_and_patterns
-from step_1.find_disentangled_system import find_disentangled_system
-
-random_number_generator = numpy.random.default_rng()
-
-
-a: float = 1
-
-print(a == 2)
+print('this is a test')
 
